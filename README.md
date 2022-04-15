cc56f827830ce18df8ef02b0d67e1474e91962e8
Christian <geissler@illusion-games.de>
2022-04-15 16:18:26 +0200

    Updated the readme

--- README.md
+++ README.md
@@ -1,2 +1,66 @@
 # driftapi
-A rest api for submitting sturmkind drift racing information
+An openapi proposal for submitting sturmkind drift racing information.
+
+This folder contains:
+main.py - A fastapi based sketch that you can run via python.
+openapi.json - for your convenience, the generated openapi.json file that you get if you run the main.py app with uvicorn and go to <yourcomputer>:<yourport>/openapi.json
+
+# vision & use case
+The basic idea behind this api is to establish a standard for the dr!ft app to inform third-party apps during a race about the progress. Ideally, the players will enter the uri of the webservice where this api is implemented on at the beginning / in the settings of the app. When they start a game and such an uri is set, the app then sends information such as when a barcode has been detected to the api. Behind the api, the webserver can use the information to provide a lap counter or the fastest lap times overview and much more.
+
+For security reasons, the whole interaction is considered to be one way. Under no circumstances should the webserver be able to influence the app and the app should always expect the webserver to be malicious. Therefore, all calls should be done in an asynchrone way and the app should never expect to get anything back for the calls.
+
+# detailed use case
+## 1 Step ##
+first, the user enters a uri into the dr!ft app settings and chooses a displayed user name. The uri is a combination of the adress where the webservice is located plus an identifier to allow the same webservice to distinguis different races. Example for such an uri:
+
+"https://driftevent.de/timeboard/race143"
+
+Where "https://driftevent.de/timeboard" is the root where the service runs, and "race143" is an identifier for a specific race, event, challenge or whatever.
+
+In the beginning, this uri will be entered manually in the app, at a future stage, a QR-Code reader could be used to set it more convenient.
+
+## 2nd Step ## (Optional)
+After setting the uri, the app can call the following uri to check if it is correct and a service is running on the other side.
+https://driftevent.de/timeboard/race143/ping
+
+## 3rd Step ##
+Then, the user just starts a normal race/gymkhana/free run as usual. Upon entering the game but before the user starts the motor, the drift app in our example can call
+
+https://driftevent.de/timeboard/race143/events/enter
+
+to submit the car and race settings as well as the user name that can be displayed on a score board. For legal / privacy reasons, this choosen user name does not need to be identical with the sturmkind user name.
+
+Note: All the calls to /events/* require a timestamp (down to fractions of seconds) and a uuid. The timestamp can be based on the system time of the device, as only the difference between the timestamps is required to calculate the lap times etc., so it is not important to exactly sync it. The uuid needs to stay the same for the race but does and must not be the sturmkind account name. One idea for it would be to generate a random cryptic uuid whenever the user enters a new uri in step 1.
+
+## 4th Step ## (Optional)
+When the player starts the motor of the car, the drift app in our example calls
+https://driftevent.de/timeboard/race143/events/startmotor
+
+## 5th Step ##
+Whenever the app recognizes that a barcode has been detected, it calls 
+
+https://driftevent.de/timeboard/race143/events/barcode
+
+See the api documentation for the required body. This tells the server when and which barcode has been crossed, which allows for counting laps, lap times etc.
+This is the most important API call. Note: for now, no further information that that is passed, maybe in the future, also the speed and drift angle might get submitted as well.
+
+## 6th Step ## (Optional)
+Whenever the app awards the player with points (positive or negative), it calls 
+
+https://driftevent.de/timeboard/race143/events/points
+
+and submits the reason, the number of points awarded for this reason and the total sum of current points. This allows a scoreboard to not just display lap times, but could also show "early start" fails and a gymkhana scoreboard.
+
+
+# installation
+pip install "fastapi[all]"
+
+# run
+uvicorn main:app --reload
+
+# using this code
+After running, a local webserver should be open and accessible via the displayed adress in the console that you used to start the program. Just open a webbrowser and go to the displayed adress. A small welcome message should appear. Go to /docs to browse the interface description. You can also download an openapi.json description of the interface that can be used in code-generators etc. to generate a server stub implementing this interface.
+
+# whats next?
+This is just a proposal to sharpen the discussion. Please check the concept and feel free to send me any questions to: geissler@illusion-games.de. After we have settled on the api, I can implement a simple webserver based on it and then, the uri-functionality and api calls can be implemented in the sturmkind app.
--- main.py
+++ main.py
@@ -4,14 +4,8 @@ from datetime import datetime
 from enum import Enum
 
 from fastapi import FastAPI
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel, ValidationError, Field
 
-class Barcode(str, Enum):
-    finish = "FINISH"
-    angledrift = "ANGLE"
-    speeddrift = "SPEED"
-    threesixty = "360"
-    hundredeightyspeed = "180SPEED"
 
 class Carsetup(str, Enum):
     drift = "DRIFT"
@@ -28,43 +22,39 @@ class Tires(str, Enum):
     spikes = "SPIKES"
     rally = "RALLY"
 
+class RaceEvent(BaseModel):
+    uuid: UUID = Field(None, title="unique user id", description="unique identifier (for the duration of the current race), can and should be different from the Sturmkind user name for legal and security reasons, for example a hash of the username or a hash of the devices ip address.")
+    timestamp: datetime = Field(None, title="the exact timestamp down to the precision the sturmkind app uses, so fractions of a second")
 
-class RacerEvent(BaseModel):
-    uuid: UUID #user name in drift app
-    timestamp: datetime
+class Barcode(str, Enum):
+    finish = "FINISH"
+    angledrift = "ANGLE"
+    speeddrift = "SPEED"
+    threesixty = "360"
+    hundredeightyspeed = "180SPEED"
 
-# This event is triggered when a user enters a server uri in the app. The app can see if there actually is a server behind that uri and could for example show a green light, so that the user knows he entered the right server.
-class HelloEvent(RacerEvent):
-    pass
-
-# This event is triggered when the user hits the "Start Motor" button the first time.
-class StartMotorEvent(RacerEvent):
-    pass
-
-# This event is triggered whenever a target is recognized or a new measurement of a multi-measurement barcode is triggered (180 speed or 360), in which case the stage parameter is provided, starting from 0
-class BarcodeEvent(RacerEvent):
-    barcode: Barcode
-    currentspeed: float
-    currentdriftangle: float
-    stage:Optional[int] = 0
-
-
-# Optional Advanced Events:
-
-# This event is triggered when the user starts a run (free run, race, gymkhana) and after the loading is completed (the user sees the hud of the racer)
-# it's purpose is for the server to control the car setup and if that matches with what is allowed for the race
-class EnterEvent(RacerEvent):
-    carname: str #Whatever the user choosed
-    cartype: str #according to the app-internal id for the different car types. No ENUM because we do not want to adjust this every time a new model comes out
-    motortype: str #according to the app-internal id for the different motor types. No ENUM for the above reason.
-    motorsetup: str #according to the app-internal id for the different motor setups. No ENUM for the above reason.
+class EnterEvent(RaceEvent):
+    username: str = Field(None, title="the name choosen by the user to be displayed on the scoreboard", description="Can be different from the Sturmkind user name (for legal reasons)")
+    carname: str = Field(None, title="the name the user has given for the specific car (not always the same as the car type)")
+    cartype: str = Field(None, title="The name of the car model, for example D1, 190 Evo II, etc., this is not an ENUM as new cars might be added in the future and we do not want to adjust the API every time that happens.")
+    motortype: str = Field(None, title="The id of the motor type. No ENUM for the above reason. Example: 'DTM', 'V8' etc.") 
+    motorsetup: str = Field(None, title="The id of the motor setup. No ENUM for the above reason. Example: 'DTM', 'V8' etc.")  #according to the app-internal id for the different motor setups. No ENUM for the above reason.
     carsetup:Carsetup #'DRIFT or RACE'
     trackproperty:Trackproperty #'DRY or WET or GRAVEL or ICE'
     tires:Tires #STREET, SPIKES, RALLY
     mode:Optional[str] = 'NONE' #NONE or RALLYCROSS
-    rounds:Optional[int] = 0
+    laps:Optional[int] = 0
     driftassist:bool
     softsteering:bool
+
+class StartMotorEvent(RaceEvent):
+    pass
+
+class BarcodeEvent(RaceEvent):
+    barcode: Barcode
+    #currentspeed: float #optional for a later implementation as discussed with martin
+    #currentdriftangle: float #optional for a later implementation as discussed with martin
+    #stage:Optional[int] = 0 #optional for a later implementation as discussed with martin
 
 class PointReasons(str, Enum):
     earlystart = "EARLYSTART"
@@ -74,8 +64,7 @@ class PointReasons(str, Enum):
     hundredeightyspeed = "180SPEED"
     intimefinish = "INTIMEFINISH"
 
-# This event is triggered whenever the app awards the user with points (also for negative points or in-time-finish)
-class PointsAwardedEvent(RacerEvent):
+class PointsAwardedEvent(RaceEvent):
     points:int
     pointsTotal:int
     reason:PointReasons
@@ -84,7 +73,7 @@ class PointsAwardedEvent(RacerEvent):
     
 
 # This event is triggered whenever the user leaves a run (shutting down the motor)
-class FinishEvent(RacerEvent):
+class FinishEvent(RaceEvent):
     pass
 
 
@@ -92,20 +81,30 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Welcome to the Dr!ft API Demo. To see the available api calls, visit /docs."}
+    return {"message": "Welcome to the Sturmkind Dr!ft Multiplayer Racing API. To see the available api calls, visit /docs."}
 
-@app.post("/event/startmotor")
-async def create_StartMotorEvent(startMotorEvent:StartMotorEvent):
-    return startMotorEvent
+# This event is triggered when a user enters a server uri in the app. The app can see if there actually is a server behind that uri and could for example show a green light, so that the user knows he entered the right server.
+@app.post("/{race_id}/ping")
+async def ping(race_id:str):
+    return {"message": "Welcome! There is a server with a race ready and waiting for your events."}
 
-@app.post("/event/barcode")
-async def create_BarcodeEvent(barcodeEvent:BarcodeEvent):
-    return barcodeEvent
-
-@app.post("/event/enter")
-async def create_BarcodeEvent(enterEvent:EnterEvent):
+# This event is triggered when the user starts a run (free run, race, gymkhana) and after the loading is completed (the user sees the hud of the racer)
+# it's purpose is for the server to control the car setup and if that matches with what is allowed for the race
+@app.post("/{race_id}/events/enter")
+async def create_EnterEvent(race_id:str, enterEvent:EnterEvent):
     return enterEvent
 
-@app.post("/event/points")
-async def create_BarcodeEvent(pointsAwardedEvent:PointsAwardedEvent):
+# This event is triggered when the user hits the "Start Motor" button the first time.
+@app.post("/{race_id}/events/startmotor")
+async def create_StartMotorEvent(race_id:str, startMotorEvent:StartMotorEvent):
+    return startMotorEvent
+
+# This event is triggered whenever a target is recognized
+@app.post("/{race_id}/events/barcode")
+async def create_BarcodeEvent(race_id:str, barcodeEvent:BarcodeEvent):
+    return barcodeEvent
+
+# This event is triggered whenever the app awards the user with points (also for negative points or in-time-finish)
+@app.post("/{race_id}/events/points")
+async def create_PointsAwardedEvent(race_id:str, pointsAwardedEvent:PointsAwardedEvent):
     return pointsAwardedEvent
